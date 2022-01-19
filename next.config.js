ad42751b4e5faf79d509b9503515ca5efa6487ac
AmrHE <amr.hassan.emam@gmail.com>
2022-01-19 18:49:17 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
-const withPlugins = require('next-compose-plugins');
-const optimizedImages = require('next-optimized-images');
+const withPlugins = require("next-compose-plugins");
+const optimizedImages = require("next-optimized-images");
 
-module.exports = withPlugins([optimizedImages], { target: 'serverless' });
+module.exports = withPlugins([optimizedImages], { target: "serverless" });
+
+const isProd = process.env.NODE_ENV === "production";
+
+module.exports = {
+	// Use the CDN in production and localhost for development.
+	assetPrefix: isProd ? "https://cdn.mydomain.com" : "",
+};
+
+module.exports = {
+	basePath: "/docs",
+};
